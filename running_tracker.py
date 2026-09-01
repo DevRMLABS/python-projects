71f514f09b89dceab49aa3a95997214122b0efb9
DevRMLABS <rileymoxham22@gmail.com>
2026-09-01 13:12:41 -0500

    Fix print statement and calculate average pace

--- running_tracker.py
+++ running_tracker.py
@@ -1,1 +1,5 @@
-print("Running Tracker")
+print("running tracker")
+distance = float(input("How many miles did you run?"))
+minuets = float(input("How many minuets did it take you to run that distance?"))
+Average_Pace = minuets / distance
+print(Average_Pace)
