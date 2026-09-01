c3603d6d1c072bffddef3931817828e0212a675d
DevRMLABS <rileymoxham22@gmail.com>
2026-09-01 13:14:05 -0500

    Fix variable name from 'minuets' to 'minutes

--- running_tracker.py
+++ running_tracker.py
@@ -1,5 +1,5 @@
 print("running tracker")
 distance = float(input("How many miles did you run?"))
-minuets = float(input("How many minuets did it take you to run that distance?"))
-Average_Pace = minuets / distance
+minutes = float(input("How many minutes did it take you to run that distance?"))
+Average_Pace = minutes / distance
 print(Average_Pace)
