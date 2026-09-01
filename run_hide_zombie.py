7cd5500e7f0c27beabaadeb56e1e1b77f73784d9
DevRMLABS <rileymoxham22@gmail.com>
2026-09-01 13:37:44 -0500

    Add interactive zombie survival choice game
    The script allows the user to choose a room to hide in during a zombie apocalypse scenario, with different outcomes based on the choice.

---
+++ run_hide_zombie.py
@@ -0,0 +1,9 @@
+# The user enters a dark and mysterious survivor house in the middle of town panic sets in, he needs to hide!
+print("You enter a small yellow survivor house, you need to hide quickly which room will you take")
+choice = input("You have three options in the house. Do you choose the mainroom, livingroom, or bathroom?")
+if choice == "mainroom":
+   print("you enter mainroom heart beating heavily you pick up your bloody crowbar and hold your ground as long as your can, soon perishing.")
+elif choice == "livingroom":
+    print("as you run for your handgun you trip on the rug smashing your head on the table ending up torn apart")
+elif choice == "bathroom":
+    print(" you run to the bathroom door slaming it shut and locking it, looking for another escape option you open the side window and make a run for it, escaping with mild fatigue but unscathed and breathing good job you have been spared!")
