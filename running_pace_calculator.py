2b87d628f365cabbcb13133ec43f7672c8b83b88
DevRMLABS <rileymoxham22@gmail.com>
2026-09-09 16:12:08 -0500

    Add running pace calculator script
    This program calculates the average running pace based on distance and time.

---
+++ running_pace_calculator.py
@@ -0,0 +1,5 @@
+# This program asks for running distance in miles, running distance in minutes, and to calculate average pace.
+miles = float(input("How many miles did you run?"))
+minutes = float(input("How many minutes did it take to run that distance?"))
+pace = minutes / miles
+print('Your pace was' , + pace, "per mile")
